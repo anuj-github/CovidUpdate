26da427cb0da8841a88ed9a390e5902856c459ba
anuj-dialpad <ajha@dialpad.com>
2020-05-01 19:36:44 +0530

    Add pie chart

---
+++ app/src/main/java/com/graduateguy/covid/util/GlobalUtil.kt
@@ -0,0 +1,17 @@
+package com.graduateguy.covid.util
+
+import com.github.mikephil.charting.utils.ColorTemplate
+
+object GlobalUtil {
+
+    public fun getColor():IntArray{
+        var arr = IntArray(3)
+        // Blue
+        arr[0] = ColorTemplate.rgb("#ff33b5e5")
+        // Green
+        arr[1]= ColorTemplate.rgb("#ff99cc00")
+        // Red
+        arr[2]= ColorTemplate.rgb("#ffff4444")
+        return arr;
+    }
+}
